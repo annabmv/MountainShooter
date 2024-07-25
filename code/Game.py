6b100d066d4f1151a65d26f8191bd0fb0e043247
annabmv <vercesiam@gmail.com>
2024-07-25 12:00:04 -0300

    Adding background and game title Creating asset folder

--- code/Game.py
+++ code/Game.py
@@ -1,20 +1,15 @@
 import pygame
 
+from code.Const import WIND_WIDTH, WIND_HEIGHT
 from code.Menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIND_WIDTH, WIND_HEIGHT))
 
     def run(self, ):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # Check for all events
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #        pygame.quit()  # close window
-            #        quit()  # end game
