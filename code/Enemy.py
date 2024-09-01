13321ab3f61341e9a39b7e85480451c8f5c991f9
annabmv <vercesiam@gmail.com>
2024-09-01 13:41:10 -0300

    Add Level 3 and Enemy 3

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,4 +1,4 @@
-from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIND_HEIGHT
 from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 
@@ -7,9 +7,24 @@ class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+        self.vertical_direction = 1  # 1 for moving down, -1 for moving up
 
     def move(self):
-        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.name == 'Enemy3':
+            # Move horizontally from right to left
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+            # Move vertically with a "bounce" effect
+            self.rect.centery += ENTITY_SPEED[self.name] * self.vertical_direction
+
+            # Change direction if hitting the screen boundaries
+            if self.rect.top <= 0:
+                self.vertical_direction = 1  # Move down
+            elif self.rect.bottom >= WIND_HEIGHT:
+                self.vertical_direction = -2  # Move up faster
+
+        else:
+            # Default movement for other enemies
+            self.rect.centerx -= ENTITY_SPEED[self.name]
 
     def shoot(self):
         self.shot_delay -= 1
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -23,6 +23,12 @@ class EntityFactory:
                     list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level2Bg{i}', (WIND_WIDTH, 0)))
                 return list_bg
+            case 'Level3Bg':
+                list_bg = []
+                for i in range(5):  # level3bg images number
+                    list_bg.append(Background(f'Level3Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level3Bg{i}', (WIND_WIDTH, 0)))
+                return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIND_HEIGHT / 2 - 30))
             case 'Player2':
@@ -31,3 +37,5 @@ class EntityFactory:
                 return Enemy('Enemy1', (WIND_WIDTH + 10, random.randint(40, WIND_HEIGHT - 40)))
             case 'Enemy2':
                 return Enemy('Enemy2', (WIND_WIDTH + 10, random.randint(40, WIND_HEIGHT - 40)))
+            case 'Enemy3':
+                return Enemy('Enemy3', (WIND_WIDTH + 10, random.randint(40, WIND_HEIGHT - 40)))
